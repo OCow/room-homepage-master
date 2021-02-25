946bcd94123875fcc62a20647432027a5b3e6779
Oliver Kuhn <oliver.kuhn@hella-gutmann.com>
2021-02-25 06:24:53 +0100

    change text of main-2, this is the page content with the show now button

--- index.js
+++ index.js
@@ -32,13 +32,18 @@ window.onload = () => {
 
 function onClickBtnLeft(e) {
   console.log("onClickBtnLeft() called...");
+
+  //the order matters! First change background, then change page
+  //Change page relies on the global index variable which is
+  //set by changeMain1Background...()
   changeMain1BackgroundBackward();
-  changeMain2PageBackward();
+  changeMain2Page();
 }
 
 function onClickBtnRight(e) {
   console.log("onClickBtnRight() called...");
   changeMain1BackgroundForward();
+  changeMain2Page();
 }
 
 function changeMain1BackgroundBackward() {
@@ -61,4 +66,42 @@ function changeMain1BackgroundForward() {
     main_1_backgrounds[main_1_backgrounds_currentIndex];
 }
 
-function changeMain2PageBackward() {}
+/**
+ * This function relies on main_1_backgrounds_currentIndex being the actual
+ * index for displaying the appropriate page. Therefore, the order in which
+ * changeMain1Background...() and changeMain2Page...() are invoked matters!
+ */
+function changeMain2Page() {
+  let page_1 = document.getElementsByClassName("page-1")[0];
+  let page_2 = document.getElementsByClassName("page-2")[0];
+  let page_3 = document.getElementsByClassName("page-3")[0];
+
+  page_1.classList.add("no-display");
+  page_2.classList.add("no-display");
+  page_3.classList.add("no-display");
+
+  switch (main_1_backgrounds_currentIndex) {
+    case 0:
+      {
+        page_1.classList.remove("no-display");
+      }
+      break;
+
+    case 1:
+      {
+        page_2.classList.remove("no-display");
+      }
+      break;
+
+    case 2:
+      {
+        page_3.classList.remove("no-display");
+      }
+      break;
+
+    default:
+      console.log(
+        `main_1_backgrounds_currentIndex=${main_1_backgrounds_currentIndex} - invalid`
+      );
+  }
+}
