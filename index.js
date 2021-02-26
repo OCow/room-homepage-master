fdc98ad0b9e11a3f7fed57e6dfea896e5b9d300c
oliver.kuhn2@gmail.com <oliver.kuhn2@gmail.com>
2021-02-26 11:40:54 +0100

    add backdrop, add click handling, currently not 100% clean when responsive switching

--- index.js
+++ index.js
@@ -25,10 +25,64 @@ window.onload = () => {
     .getElementById("navigation-button-right-desktop")
     .addEventListener("click", onClickBtnRight);
 
+  document.getElementById("mmbtn").addEventListener("click", onClickMenuButton);
+
+  document
+    .getElementById("mm-backdrop")
+    .addEventListener("click", onClickBackdrop);
+
+  window.addEventListener("resize", onWindowResize);
+
   let main_1 = document.getElementsByClassName("main-1")[0];
   main_1.style.backgroundImage =
     main_1_backgrounds[main_1_backgrounds_currentIndex];
 };
+
+function onWindowResize(e) {
+  // console.log(e);
+  console.log(window.innerWidth);
+
+  //in case the width is larger or equal to 1000,
+  //check if the mobile menu is currently being shown (meaning
+  //that the burger menu was clicked). In case it is, ensure
+  //that the class is appropriately changed
+  if (window.innerWidth >= 1000) {
+    if (showMobileMenu === true) {
+      onClickMenuButton();
+    }
+  }
+}
+
+let showMobileMenu = false;
+function onClickMenuButton(e) {
+  let navbar = document.getElementById("my-navbar");
+  let img = document.getElementById("mm-img");
+  let backdrop = document.getElementById("mm-backdrop");
+
+  if (showMobileMenu === true) {
+    //switch back to showing burger menu
+    img.src = "./images/icon-hamburger.svg";
+    navbar.classList.add("nav-view-default");
+    navbar.classList.remove("nav-view-clicked");
+    backdrop.classList.add("no-display");
+  } else {
+    //switch to showing the mobile menu:
+    //- switch img from burger to cross
+    //- change nav class so that links are being shown
+    //- show backdrop
+    img.src = "./images/icon-close.svg";
+    navbar.classList.remove("nav-view-default");
+    navbar.classList.add("nav-view-clicked");
+    backdrop.classList.remove("no-display");
+  }
+
+  //toggle variable
+  showMobileMenu = !showMobileMenu;
+}
+
+function onClickBackdrop() {
+  onClickMenuButton();
+}
 
 function onClickBtnLeft(e) {
   console.log("onClickBtnLeft() called...");
